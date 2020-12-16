144b93572bf909b361bf023057daec95dfc03eff
Debassih Sahoo <debasishsahoo8523@gmail.com>
2020-12-16 17:45:51 +0530

    Added inline comments

--- consumer.py
+++ consumer.py
@@ -1,9 +1,12 @@
 import pika
 
+# Connect to RabbitMQ server
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
-
 channel = connection.channel()
+
+# Declare the queue in case it doesn't already exist
+channel.queue_declare(queue='hello')
 
 
 def callback(ch, method, properties, body):
@@ -14,6 +17,6 @@ channel.basic_consume(
     queue='hello', on_message_callback=callback, auto_ack=True
 )
 
-print('[*] Waiting for messages to exit press ctrl + c')
+print('[*] Waiting for messages to exit press CTRL+C')
 
 channel.start_consuming()
--- producer.py
+++ producer.py
@@ -1,12 +1,15 @@
 import pika
 
+# Connect to RabbitMQ server
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
-
 channel = connection.channel()
 
+# Create a new queue called hello
 channel.queue_declare(queue='hello')
 
+# Add a message to the hello queue
 channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
 
+# Close the connection
 connection.close()
